fcbea326aea4c86bf7aec7a44ae941df3e0d1420
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-10 23:14:18 +0400

    working server POST method

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const periods = require('./periodsData');
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/api/periods', (req, res) => res.send(periods))
+  .post('/api/periods', (req, res) => res.send(req.body))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// TODO: Add nodemon.
