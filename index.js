fea0aae9fafdf9a5fbf9708ad5cc3fb72f49788b
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-10 15:55:05 +0400

    fix:rename port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 8081;
+const PORT = 8081;
 const periods = require('./periodsData');
 
 express()
