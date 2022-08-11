00ca868c44d59787a260d0908d8361af46c1d790
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-11 17:37:06 +0400

    test

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-const express = require('express');
+// const express = require('express');
 
 const periodsData = [
   { id: 1, type: 'Had flows', date: '2022-08-10' },
   { id: 2, type: 'Meds', date: '2022-08-14' },
 ];
 
+// const app = express();
+// app.use(express.json());
+
+// const port = 8081;
+
+// app.get('/api/periods', function (req, res) {
+//   res.json(periodsData);
+// });
+
+// app.post('/api/periods', (req, res) => {
+//   periodsData.push(req.body);
+//   res.json(req.body);
+// });
+
+// app.listen(port, function () {
+//   console.log(`Listening to requests on http://localhost:${port}`);
+// });
+
+// create an express app
+const express = require('express');
 const app = express();
-app.use(express.json());
 
-const port = 8081;
+// use the express-static middleware
+app.use(express.static('public'));
 
+// define the first route
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World!</h1>');
+});
 app.get('/api/periods', function (req, res) {
-  res.json(periodsData);
+  res.send(periodsData);
 });
 
-app.post('/api/periods', (req, res) => {
-  periodsData.push(req.body);
-  res.json(req.body);
-});
-
-app.listen(port, function () {
-  console.log(`Listening to requests on http://localhost:${port}`);
-});
+// start the server listening for requests
+app.listen(3000, () => console.log('Server is running...'));
