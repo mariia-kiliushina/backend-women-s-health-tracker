0a02a6299941f82f6fea39eab5b8f4c6dfbe2f02
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-11 18:55:23 +0400

    post method works

--- index.js
+++ index.js
@@ -26,7 +26,10 @@
 const express = require('express');
 var cors = require('cors');
 const PORT = process.env.PORT || 8081;
-const periodsData = require('./periodsData');
+const periodsData = [
+  { id: 1, type: 'Had flows', date: '2022-08-10' },
+  { id: 2, type: 'Meds', date: '2022-08-14' },
+];
 
 express()
   .use(cors())
