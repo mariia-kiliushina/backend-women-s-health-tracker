1fff93dcba5aca0441f959816e0de875ea6d99e8
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-11 14:07:40 +0400

    fix:upd base after post

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const periods = require('./periodsData');
+
+const periodsData = [
+  { id: 1, type: 'Had flows', date: '2022-08-10' },
+  { id: 2, type: 'Meds', date: '2022-08-14' },
+];
 
 const app = express();
 app.use(express.json());
@@ -7,10 +11,11 @@ app.use(express.json());
 const port = 8081;
 
 app.get('/api/periods', function (req, res) {
-  res.json(periods);
+  res.json(periodsData);
 });
 
 app.post('/api/periods', (req, res) => {
+  periodsData.push(req.body);
   res.json(req.body);
 });
 
