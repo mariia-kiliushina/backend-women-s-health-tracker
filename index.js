56e1fb7e9f247829396086804698e3837e6789be
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-12 16:08:02 +0400

    GET works with DB

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const express = require('express');
+var cors = require('cors');
+const PORT = process.env.PORT || 8081;
+//
 const { Client } = require('pg');
+
 const client = new Client({
   user: 'markil',
   host: 'localhost',
@@ -7,12 +12,30 @@ const client = new Client({
   port: 5432,
 });
 
+let tableName = 'periods_table';
+
+let RESULT;
+
 client.connect();
-client.query(`Select * from testdb`, (err, res) => {
+client.query(`Select * from ${tableName}`, (err, res) => {
   if (!err) {
+    RESULT = res.rows;
     console.log(res.rows);
   } else {
     console.log(err.message);
   }
   client.end;
 });
+
+///
+
+express()
+  .use(cors())
+  .use(express.json())
+  .get('/', (req, res) => res.send('<h1>Hello from backend</h1>'))
+  .get('/api/periods', (req, res) => res.send(RESULT))
+  .post('/api/periods', (req, res) => {
+    periodsData.push(req.body);
+    res.json(req.body);
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
