6abebd5e6f0950a77b1648139b09edda29213ee5
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-17 17:42:56 +0400

    works and pause for a while

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 const express = require('express');
 var cors = require('cors');
-const expressPORT = 8081;
-const restAPIs = require('./restAPI');
+const PORT = process.env.PORT || 8081;
+
+const { Client } = require('pg');
 const { config } = require('dotenv');
 
 config();
 
-const app = express();
+const client = new Client({
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  password: process.env.PASSWORD,
+  port: 5432,
+  user: process.env.USER_NAME,
+});
 
-app
+let tableName = 'periods_table';
+
+client.connect();
+
+express()
   .use(cors())
   .use(express.json())
   .get('/', (req, res) => res.send('<h1>Hello from backend</h1>'))
-  .get('/api/periods', restAPIs.getDBdata)
-  .post('/api/periods', restAPIs.postDBdata)
-  .listen(expressPORT, () => console.log(`Listening on ${expressPORT}`));
-// .get('/api/users/:id', restAPIs.getUserById)
+  .get('/api/periods', (request, response) => {
+    client.query(`Select * from ${tableName}`, (error, result) => {
+      if (error) {
+        throw error;
+      }
+      response.json(result.rows);
+    });
+  })
+  .post('/api/periods', (request, response) => {
+    const { type, date } = request.body;
+    client.query(
+      `INSERT INTO ${tableName} ( type, date) VALUES ($1,$2)`,
+      [type, date],
+      (error, result) => {
+        if (error) {
+          throw error;
+        }
+        client.query(`SELECT * from ${tableName} ORDER BY id DESC LIMIT 1`, (error, result) => {
+          if (error) {
+            throw error;
+          }
+          response.json(result.rows);
+        });
+      }
+    );
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
